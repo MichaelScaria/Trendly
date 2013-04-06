d00551f2d0ac39614da71a65c1835615e27cc45c
Michael Scaria <michaelscaria@yahoo.com>
2013-04-06 14:33:41 -0500

    Initial Commit

---
+++ Trendly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Trendly
+//
+//  Created by Michael Scaria on 4/6/13.
+//  Copyright (c) 2013 Michael Scaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Trendly/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Trendly
+//
+//  Created by Michael Scaria on 4/6/13.
+//  Copyright (c) 2013 Michael Scaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
